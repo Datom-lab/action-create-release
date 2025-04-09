b8dd878aa8578f2dd48af9d7cc10f2f0904aadba
Chema <jmpujades.dev@proton.me>
2025-04-09 20:22:11 +0200

    Add GitHub Action and release script for automated releases

---
+++ release.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+VERSION=$INPUT_VERSION
+PR_BODY=$INPUT_PR_BODY
+GITHUB_TOKEN=$INPUT_GITHUB_TOKEN
+GITHUB_REPOSITORY=$INPUT_GITHUB_REPOSITORY
+GITHUB_API_URL="https://api.github.com"
+GITHUB_RELEASE_URL="$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/releases"
+
+
+git config --global user.name "Datom Actions"
+git config --global user.email "actions@datom.com"
+
+echo "Creating tag for version $VERSION"
+git tag -a "$VERSION" -m "Release $VERSION"
+git push origin "$VERSION"
+
+
+description=$(echo "$PR_BODY" | jq -Rs .)
+echo "Creating release for version $VERSION"
+curl -X POST -H "Authorization: token $GITHUB_TOKEN" \
+     -H "Accept: application/vnd.github.v3+json" \
+     -d '{
+           "tag_name": "'"$VERSION"'",
+           "target_commitish": "main",
+           "name": "'"$VERSION"'",
+           "body": '"$description"',
+           "draft": false,
+           "prerelease": false
+         }' \
+     "$GITHUB_RELEASE_URL"
